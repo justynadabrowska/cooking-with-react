e3a37044b6d9231870193e744ed2f36434cc880d
justynadabrowska <74719040+justynadabrowska@users.noreply.github.com>
2021-12-07 17:45:40 +0000

    component breakdown and props

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,31 +1,33 @@
-import React from 'react'
+import React from "react";
 
-export default function Recipe() {
-    return (
+export default function Recipe(props) {
+    const { 
+        name,
+        cookTime,
+        servings,
+        instructions
+    } = props
+  return (
+    <div>
+      <div>
+        <h3>{name}</h3>
         <div>
-            <div>
-                <h3>Plain Chicken</h3>
-                <div>
-                    <button>Edit</button>
-                    <button>Delete</button>
-                </div>
-            </div>
-            <div>
-                <span>Cook Time:</span>
-                <span>1:45</span>
-            </div>
-            <div>
-                <span>Servings:</span>
-                <span>3</span>
-            </div>
-            <div>
-                <span>Instructions</span>
-                <div>
-                    1. Put salt on chicken
-                    2. Put chicken in oven
-                    3. Eat chicken
-                </div>
-            </div>
+          <button>Edit</button>
+          <button>Delete</button>
         </div>
-    )
+      </div>
+      <div>
+        <span>Cook Time:</span>
+        <span>{cookTime}</span>
+      </div>
+      <div>
+        <span>Servings:</span>
+        <span>{servings}</span>
+      </div>
+      <div>
+        <span>Instructions</span>
+        <div>{instructions}</div>
+      </div>
+    </div>
+  );
 }
